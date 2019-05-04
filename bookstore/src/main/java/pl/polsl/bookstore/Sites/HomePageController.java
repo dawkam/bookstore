55e536d3ed71ec814461740e561fbe036b427aae
KingBaum <bergerfranek@gmail.com>
2019-05-04 23:58:47 +0200

    Added home page css and html files and the homePage class.

---
+++ bookstore/src/main/java/pl/polsl/bookstore/Sites/HomePageController.java
@@ -0,0 +1,28 @@
+package pl.polsl.bookstore.Sites;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/home")
+public class HomePageController {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+
+}
