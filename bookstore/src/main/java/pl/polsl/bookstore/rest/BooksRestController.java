014c54a5a59167d141cd792bba43a06d112d4623
Dawid <dawid.kampa@wp.pl>
2019-05-04 21:30:55 +0200

    poprawka do bookRestControllera

--- bookstore/src/main/java/pl/polsl/bookstore/rest/BooksRestController.java
+++ bookstore/src/main/java/pl/polsl/bookstore/rest/BooksRestController.java
@@ -1,11 +1,14 @@
 package pl.polsl.bookstore.rest;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.polsl.bookstore.entity.Books;
 import pl.polsl.bookstore.repository.BooksRepository;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -15,4 +18,11 @@ public class BooksRestController {
 
     @Autowired
     public BooksRestController(BooksRepository theBooksRepository ){    booksRepository =  theBooksRepository;  }
+
+
+    // expose "/books" and return list of books
+    @GetMapping("/books")
+    public List<Books> findAll(){return booksRepository.findAll();}
+
+
 }
