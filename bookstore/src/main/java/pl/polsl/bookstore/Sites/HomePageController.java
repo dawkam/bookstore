85c22f5ed7a8c468be52c30546fe888837a763cd
KingBaum <bergerfranek@gmail.com>
2019-05-05 21:59:41 +0200

    Developed the homePage and added the ProfilePage.html for testing purposes.

--- bookstore/src/main/java/pl/polsl/bookstore/Sites/HomePageController.java
+++ bookstore/src/main/java/pl/polsl/bookstore/Sites/HomePageController.java
@@ -7,21 +7,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/home")
+//@RequestMapping("/home")
 public class HomePageController {
 
     @Autowired
     private HttpServletRequest request;
 
-    @GetMapping("/greeting")
+    @GetMapping("/home")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "home";
+    }
+
+    @RequestMapping("/profile")
+    public String goToProfile(){
+        return "profile";
     }
 
 
