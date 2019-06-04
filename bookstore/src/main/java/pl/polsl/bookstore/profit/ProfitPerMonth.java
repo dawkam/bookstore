992f2879fd1dc8d28b5e243123db307a5815fdf6
Dawid <dawid.kampa@wp.pl>
2019-06-04 20:49:29 +0200

    profit

---
+++ bookstore/src/main/java/pl/polsl/bookstore/profit/ProfitPerMonth.java
@@ -0,0 +1,41 @@
+package pl.polsl.bookstore.tmp;
+
+public class ProfitPerMonth {
+
+    private int year;
+    private int month;
+    private float profit;
+
+    public ProfitPerMonth(int year, int month, float profit) {
+        this.year = year;
+        this.month = month;
+        this.profit = profit;
+    }
+
+    public ProfitPerMonth() {
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public float getProfit() {
+        return profit;
+    }
+
+    public void setProfit(int profit) {
+        this.profit = profit;
+    }
+}
